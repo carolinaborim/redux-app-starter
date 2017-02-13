b81d67d148f64e0987eacc95399c0714e655978a
Carolina Borim <carolina.borim@gmail.com>
2017-02-12 19:35:50 -0500

    Creates an ActionCreator, and the steps to connect the App with this action creator, returning the name of the book when the user click on it

--- src/containers/bookList.js
+++ src/containers/bookList.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index'; //import the action creator
+import {bindActionCreators} from 'redux';
 
-export default class BookList extends Component{
+class BookList extends Component{
   renderList(){
     return this.props.books.map((book) => {
       return(
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
+
   render(){
     return(
       <ul className="list-group col-sm-4">
@@ -16,3 +25,18 @@ export default class BookList extends Component{
     );
   }
 }
+
+function mapStateToProps(state){
+  return{
+    books: state.books
+  };
+}
+
+//Anything returned from this function will end up as props on the BookList Container
+function mapDispatchToProps(dispatch){
+//Whenever selectBook is called, the result should be passer to all reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, SelectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
